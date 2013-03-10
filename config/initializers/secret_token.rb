72a954d632401360440e3326f81fa52acd62ce09
Ryan Wold <wold@afomi.com>
2013-03-09 16:51:43 -0800

    initial commit on rails 3.2.12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmap::Application.config.secret_token = '9de08e487f28c5999caad1099d70bb7f43cbd3279bd49e58030c3091125922e62dcb9f393cbfd530669ff25a88e9f510f67a7d7e60c2b6babc0427ba63efb2c8'
